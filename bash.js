dcc4426ca872c7ef2cd8e3985a2e03ff96f09d40
Tate Redding <tateredding@gmail.com>
2023-02-20 12:38:17 -0700

    made pwd a module and added ls module

--- bash.js
+++ bash.js
@@ -1,12 +1,13 @@
 // Output a prompt
 process.stdout.write("prompt > ");
+const pwd = require("./pwd");
 
 // The stdin "data" event fires after a user types in a line
 process.stdin.on("data", (data) => {
     const cmd = data.toString().trim(); // remove the newline
 
     if (cmd === "pwd") {
-        process.stdout.write(process.cwd());
+        pwd();
     } else {
         process.stdout.write("You typed: " + cmd);
     }
---
+++ pwd.js
@@ -0,0 +1,6 @@
+const pwd = () => {
+    process.stdout.write(process.cwd());
+    process.stdout.write("prompt > ");
+};
+
+module.exports = pwd;
